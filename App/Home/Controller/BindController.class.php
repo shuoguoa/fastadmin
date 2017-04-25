7d63391857be39c220bfcceba279c4354a9ee679
godsoul <godsoul1986@gmail.com>
2017-04-25 18:08:21 +0800

    m

--- App/Home/Controller/BindController.class.php
+++ App/Home/Controller/BindController.class.php
@@ -16,30 +16,29 @@ class BindController extends ComController {
         $os = I('os');
         $userModel = D('User');
         $userInfo = $userModel->getUserInfo($username, $os);
-  
+
         if( $userInfo && md5(md5( $password ) . $userInfo['salt']) == $userInfo['password'] ){
             //检查是否已被其他微信账号绑定
             $wxBindModel = D('WxBind');
             $result = $wxBindModel->where(array('uid'=>$userInfo['id']))->find();
             if($result){
-                echo "<script>alert('该账号已被绑定，请解绑后再试！');</script>";
-            }
-            $data['openid'] = $openid;
-            $data['uid'] = $userInfo['id'];
-            $data['os'] = $userInfo['os'];
-            $data['bind_time'] = time();
-            $data['status'] = 1;
-            $data['ccode'] = $ccode;
-            $data['username'] = $userInfo['username'];
-            if( $wxBindModel->add($data) ){
-                $this->redirect('Index/index');
-               // session('openid', $openid);
-            }else{
-                $this->error(L('绑定失败，请重试'));
+                echo "<script>alert('该账号已被绑定，请解绑后再试！');history.back();</script>";
+            } else {
+                $data['openid'] = $openid;
+                $data['uid'] = $userInfo['id'];
+                $data['os'] = $userInfo['os'];
+                $data['bind_time'] = time();
+                $data['status'] = 1;
+                $data['ccode'] = $ccode;
+                $data['username'] = $userInfo['username'];
+                if( $wxBindModel->add($data) ){
+                    $this->redirect('Index/index');
+                }else{
+                    $this->error(L('绑定失败，请重试'));
+                }
             }
         }else{
-            echo "<script>alert('账号或密码错误');</script>";
-           // $this->success(L('账号或密码错误'));
+            echo "<script>alert('账号或密码错误');history.back();</script>";
         }
     }
 }
